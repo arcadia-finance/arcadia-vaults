a891186b63932f88480127d83e7c41ff0b7237cc
Thomas Smets <thomass-smets@hotmail.com>
2022-05-05 14:56:35 +0200

    🔥 Remove unnecessary transfer function

--- src/mockups/ERC20SolmateMock.sol
+++ src/mockups/ERC20SolmateMock.sol
@@ -5,7 +5,6 @@ import "../../lib/solmate/src/tokens/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
 
-
   constructor(string memory name, string memory symbol, uint8 _decimalsInput) ERC20(name, symbol, _decimalsInput) {
   }
 
@@ -15,15 +14,6 @@ contract ERC20Mock is ERC20 {
 
   function burn(uint256 amount) public {
       _burn(msg.sender, amount);
-  }
-
-  //Following logic added only for the paper trading competition
-  function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
-    if (from == to) {
-      return true; 
-    } else {
-      return super.transferFrom(from, to, amount);
-    }
   }
 
 }
