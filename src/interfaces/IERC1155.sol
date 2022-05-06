fac77bcb790fa93e83631e03c357a681dbc915c4
Thomas Smets <thomass-smets@hotmail.com>
2022-05-06 14:07:01 +0200

    ♻️ Refactor Token Contracts

--- src/interfaces/IERC1155.sol
+++ src/interfaces/IERC1155.sol
@@ -4,6 +4,4 @@ pragma solidity >=0.4.22 <0.9.0;
 interface IERC1155 {
   function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes calldata data) external;
   function balanceOf(address account, uint256 id) external view returns (uint256);
-  function mint(address to, uint256 id, uint256 amount) external; //function only added for the paper trading competition
-  function burn(uint256 id, uint256 amount) external; //function only added for the paper trading competition
   }
---
+++ src/paperTradingCompetition/interfaces/IERC721PaperTrading.sol
@@ -0,0 +1,12 @@
+// This is a private, unpublished repository.
+// All rights reserved to Arcadia Finance.
+// Any modification, publication, reproduction, commercialisation, incorporation, sharing or any other kind of use of any part of this code or derivatives thereof is not allowed.
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./../../interfaces/IERC721.sol";
+
+interface IERC721PaperTrading is IERC721 {
+  function mint(address to, uint256 id) external;
+  function burn(uint256 id) external;
+}
