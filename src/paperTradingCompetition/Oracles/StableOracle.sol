e4709231fba3ecdf84b756a53e72295c07761d66
zeki <zeki@arcadia.finance>
2022-05-25 15:31:17 +0200

    Fix linting in the oracle contract

--- src/paperTradingCompetition/Oracles/StableOracle.sol
+++ src/paperTradingCompetition/Oracles/StableOracle.sol
@@ -8,23 +8,25 @@ import "../../../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract StableOracle is Ownable {
 
-	uint80 private roundId;
-	int256 private answer;
-	uint256 private startedAt;
-	uint256 private updatedAt;
-	uint80 private answeredInRound;
+    uint80 private roundId;
+    int256 private answer;
+    uint256 private startedAt;
+    uint256 private updatedAt;
+    uint80 private answeredInRound;
 
-  uint8 public decimals;
-  string public description;
+    uint8 public decimals;
+    string public description;
 
-  constructor (uint8 _decimals, string memory _description) {
-		decimals = _decimals;
-		description = _description;
-		answer = int256(10 ** _decimals);
-  }
+    constructor (uint8 _decimals, string memory _description) {
+        decimals = _decimals;
+        description = _description;
+        answer = int256(10 ** _decimals);
+    }
 
-  function latestRoundData() public view returns (uint80, int256, uint256, uint256, uint80) {
-    return (roundId, answer, startedAt, updatedAt, answeredInRound);
-  }
-  
+    function latestRoundData() public view returns (uint80, int256, uint256, uint256, uint80) {
+        return (roundId, answer, startedAt, updatedAt, answeredInRound);
+    }
+
+
+
 }
