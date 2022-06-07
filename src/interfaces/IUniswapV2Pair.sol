5599297b05ed867e07652a83a96a736379591a03
Thomas Smets <thomass-smets@hotmail.com>
2022-06-07 14:43:41 +0200

    ✨ Add Uniswap V2 interfaces

--- src/interfaces/IUniswapV2Pair.sol
+++ src/interfaces/IUniswapV2Pair.sol
@@ -10,4 +10,5 @@ interface IUniswapV2Pair {
     function token0() external view returns (address);
     function token1() external view returns (address);
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function kLast() external view returns (uint);
 }
