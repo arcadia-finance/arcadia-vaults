bb5674cb56296afd0d5b810246ec70e658e1d6f1
Thomas Smets <thomass-smets@hotmail.com>
2022-06-07 11:18:05 +0200

    ✨ Add uniswap liquidity logic

--- src/AssetRegistry/UniswapV2SubRegistry.sol
+++ src/AssetRegistry/UniswapV2SubRegistry.sol
@@ -5,6 +5,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "./AbstractSubRegistry.sol";
+import "../interfaces/IUniswapV2Pair.sol";
 import {FixedPointMathLib} from '../utils/FixedPointMathLib.sol';
 
 /** 
@@ -12,16 +13,16 @@ import {FixedPointMathLib} from '../utils/FixedPointMathLib.sol';
   * @author Arcadia Finance
   * @notice The UniswapV2SubRegistry stores pricing logic and basic information for Uniswap V2 LP tokens
   * @dev No end-user should directly interact with the UniswapV2SubRegistry, only the Main-registry, Oracle-Hub or the contract owner
+  * @dev Modifications from https://github.com/Uniswap/v2-periphery/blob/267ba44471f3357071a2fe2573fe4da42d5ad969/contracts/libraries/UniswapV2LiquidityMathLibrary.sol
  */
 contract UniswapV2SubRegistry is SubRegistry {
   using FixedPointMathLib for uint256;
 
-  uint256 public constant poolUnit = 1000000000000000000; //All Uniswap V2 pool-tokens have 18 decimals
-
+  uint256 public constant poolUnit = 1000000000000000000; //All Uniswap V2 pair-tokens have 18 decimals
   struct AssetInformation {
     uint64 token0Unit;
     uint64 token1Unit;
-    address pool;
+    address pair;
     address token0;
     address token1;
   }
@@ -52,8 +53,139 @@ contract UniswapV2SubRegistry is SubRegistry {
 
     uint256[] memory tokenRates = IMainRegistry(oracleHub).getListOfValuesPerAsset(tokens, new uint256[](2), tokenUnits, getValueInput.numeraire);
 
-    
+    uint256 totalSupply = IUniswapV2Pair(assetToInformation[getValueInput.assetAddress].pair).totalSupply();
+    (uint256 token0Reserve, uint256 token1Reserve, ) = IUniswapV2Pair(assetToInformation[getValueInput.assetAddress].pair).getReserves();
 
   }
+
+    // computes the direction and magnitude of the profit-maximizing trade
+    function computeProfitMaximizingTrade(
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB,
+        uint256 reserveA,
+        uint256 reserveB
+    ) pure internal returns (bool aToB, uint256 amountIn) {
+        aToB = reserveA.mul(truePriceTokenB) / reserveB < truePriceTokenA;
+
+        uint256 invariant = reserveA.mul(reserveB);
+
+        uint256 leftSide = Babylonian.sqrt(
+            invariant.mul(aToB ? truePriceTokenA : truePriceTokenB).mul(1000) /
+            uint256(aToB ? truePriceTokenB : truePriceTokenA).mul(997)
+        );
+        uint256 rightSide = (aToB ? reserveA.mul(1000) : reserveB.mul(1000)) / 997;
+
+        if (leftSide < rightSide) return (false, 0);
+
+        // compute the amount that must be sent to move the price to the profit-maximizing price
+        amountIn = leftSide.sub(rightSide);
+    }
+
+    // gets the reserves after an arbitrage moves the price to the profit-maximizing ratio given an externally observed true price
+    function getReservesAfterArbitrage(
+        address factory,
+        address tokenA,
+        address tokenB,
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB
+    ) view internal returns (uint256 reserveA, uint256 reserveB) {
+        // first get reserves before the swap
+        (reserveA, reserveB) = UniswapV2Library.getReserves(factory, tokenA, tokenB);
+
+        require(reserveA > 0 && reserveB > 0, 'UniswapV2ArbitrageLibrary: ZERO_PAIR_RESERVES');
+
+        // then compute how much to swap to arb to the true price
+        (bool aToB, uint256 amountIn) = computeProfitMaximizingTrade(truePriceTokenA, truePriceTokenB, reserveA, reserveB);
+
+        if (amountIn == 0) {
+            return (reserveA, reserveB);
+        }
+
+        // now affect the trade to the reserves
+        if (aToB) {
+            uint amountOut = UniswapV2Library.getAmountOut(amountIn, reserveA, reserveB);
+            reserveA += amountIn;
+            reserveB -= amountOut;
+        } else {
+            uint amountOut = UniswapV2Library.getAmountOut(amountIn, reserveB, reserveA);
+            reserveB += amountIn;
+            reserveA -= amountOut;
+        }
+    }
+
+    // computes liquidity value given all the parameters of the pair
+    function computeLiquidityValue(
+        uint256 reservesA,
+        uint256 reservesB,
+        uint256 totalSupply,
+        uint256 liquidityAmount,
+        bool feeOn,
+        uint kLast
+    ) internal pure returns (uint256 tokenAAmount, uint256 tokenBAmount) {
+        if (feeOn && kLast > 0) {
+            uint rootK = Babylonian.sqrt(reservesA.mul(reservesB));
+            uint rootKLast = Babylonian.sqrt(kLast);
+            if (rootK > rootKLast) {
+                uint numerator = totalSupply.mul(rootK.sub(rootKLast));
+                uint denominator = rootK.mul(5).add(rootKLast);
+                uint feeLiquidity = numerator / denominator;
+                totalSupply = totalSupply.add(feeLiquidity);
+            }
+        }
+        return (reservesA.mul(liquidityAmount) / totalSupply, reservesB.mul(liquidityAmount) / totalSupply);
+    }
+
+    // get all current parameters from the pair and compute value of a liquidity amount
+    // **note this is subject to manipulation, e.g. sandwich attacks**. prefer passing a manipulation resistant price to
+    // #getLiquidityValueAfterArbitrageToPrice
+    function getLiquidityValue(
+        address factory,
+        address tokenA,
+        address tokenB,
+        uint256 liquidityAmount
+    ) internal view returns (uint256 tokenAAmount, uint256 tokenBAmount) {
+        (uint256 reservesA, uint256 reservesB) = UniswapV2Library.getReserves(factory, tokenA, tokenB);
+        IUniswapV2Pair pair = IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
+        bool feeOn = IUniswapV2Factory(factory).feeTo() != address(0);
+        uint kLast = feeOn ? pair.kLast() : 0;
+        uint totalSupply = pair.totalSupply();
+        return computeLiquidityValue(reservesA, reservesB, totalSupply, liquidityAmount, feeOn, kLast);
+    }
+
+    // given two tokens, tokenA and tokenB, and their "true price", i.e. the observed ratio of value of token A to token B,
+    // and a liquidity amount, returns the value of the liquidity in terms of tokenA and tokenB
+    function getLiquidityValueAfterArbitrageToPrice(
+        address factory,
+        address tokenA,
+        address tokenB,
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB,
+        uint256 liquidityAmount
+    ) internal view returns (
+        uint256 tokenAAmount,
+        uint256 tokenBAmount
+    ) {
+        bool feeOn = IUniswapV2Factory(factory).feeTo() != address(0);
+        IUniswapV2Pair pair = IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
+        uint kLast = feeOn ? pair.kLast() : 0;
+        uint totalSupply = pair.totalSupply();
+
+        // this also checks that totalSupply > 0
+        require(totalSupply >= liquidityAmount && liquidityAmount > 0, 'ComputeLiquidityValue: LIQUIDITY_AMOUNT');
+
+        (uint reservesA, uint reservesB) = getReservesAfterArbitrage(factory, tokenA, tokenB, truePriceTokenA, truePriceTokenB);
+
+        return computeLiquidityValue(reservesA, reservesB, totalSupply, liquidityAmount, feeOn, kLast);
+    }
+
+    // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
+        require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
+        require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
+        uint amountInWithFee = amountIn.mul(997);
+        uint numerator = amountInWithFee.mul(reserveOut);
+        uint denominator = reserveIn.mul(1000).add(amountInWithFee);
+        amountOut = numerator / denominator;
+    }
 
 }
