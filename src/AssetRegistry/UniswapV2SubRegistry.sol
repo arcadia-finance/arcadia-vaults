24245d7d4c727b893aaa03b5e40fc2f4cf55317c
Thomas Smets <thomass-smets@hotmail.com>
2022-06-08 13:11:50 +0200

    ✨ Finish mathematics and documentation

--- src/AssetRegistry/UniswapV2SubRegistry.sol
+++ src/AssetRegistry/UniswapV2SubRegistry.sol
@@ -14,7 +14,7 @@ import {FixedPointMathLib} from '../utils/FixedPointMathLib.sol';
   * @author Arcadia Finance
   * @notice The UniswapV2SubRegistry stores pricing logic and basic information for Uniswap V2 LP tokens
   * @dev No end-user should directly interact with the UniswapV2SubRegistry, only the Main-registry, Oracle-Hub or the contract owner
-  * @dev Modifications from https://github.com/Uniswap/v2-periphery/blob/267ba44471f3357071a2fe2573fe4da42d5ad969/contracts/libraries/UniswapV2LiquidityMathLibrary.sol
+  * @dev Most logic in this contract is a modifications of https://github.com/Uniswap/v2-periphery/blob/master/contracts/libraries/UniswapV2LiquidityMathLibrary.sol#L23
  */
 contract UniswapV2SubRegistry is SubRegistry {
   using FixedPointMathLib for uint256;
@@ -22,12 +22,12 @@ contract UniswapV2SubRegistry is SubRegistry {
   uint256 public constant poolUnit = 1000000000000000000; //All Uniswap V2 pair-tokens have 18 decimals
   address public immutable uniswapV2Factory;
 
+  bool feeOn;
+
   struct AssetInformation {
-    uint64 token0Unit;
-    uint64 token1Unit;
-    address pair;
-    address token0;
-    address token1;
+    address pair;   // Contract address of the Uniswap V2 Liquidity pool
+    address token0; // Contract address of token0
+    address token1; // Contract address of token1
   }
 
   mapping (address => AssetInformation) public assetToInformation;
@@ -42,11 +42,20 @@ contract UniswapV2SubRegistry is SubRegistry {
       uniswapV2Factory = _uniswapV2Factory;
   }
 
+    /**
+   * @notice Fetches boolean on the uniswap factory if fees are enabled or not
+   */
+    function syncFee() external {
+        feeOn = IUniswapV2Factory(uniswapV2Factory).feeTo() != address(0);
+    }
+
   /**
    * @notice Returns the value of a certain asset, denominated in a given Numeraire
    * @param getValueInput A Struct with all the information neccessary to get the value of an asset
    * @return valueInUsd The value of the asset denominated in USD with 18 Decimals precision
    * @return valueInNumeraire The value of the asset denominated in Numeraire different from USD with 18 Decimals precision
+   * @dev Since Uniswap V2 uses 50/50 pools, it is sufficient to only calculate the value of one of the underlying tokens of the 
+   *      liquidity position and multiply with 2 to get the total value of the liquidity position
    */
   function getValue(GetValueInput memory getValueInput) public view override returns (uint256 valueInUsd, uint256 valueInNumeraire) {
 
@@ -54,37 +63,141 @@ contract UniswapV2SubRegistry is SubRegistry {
     tokens[0] = assetToInformation[getValueInput.assetAddress].token0;
     tokens[1] = assetToInformation[getValueInput.assetAddress].token1;
     uint256[] memory tokenAmounts = new uint256[](2);
-    //To calculate the liquidity value after arbitrage, what matters is the ratio of the price of token0 compared to the price token1
-    //Hence we need to use the true price for an equal amount of tokens, we use 1 WAD (10**18) to guarantee precision
+    // To calculate the liquidity value after arbitrage, what matters is the ratio of the price of token0 compared to the price of token1
+    // Hence we need to use the true price for an equal amount of tokens, we use 1 WAD (10**18) to guarantee precision
     tokenAmounts[0] = FixedPointMathLib.WAD;
     tokenAmounts[1] = FixedPointMathLib.WAD;
 
     uint256[] memory tokenRates = IMainRegistry(oracleHub).getListOfValuesPerAsset(tokens, new uint256[](2), tokenAmounts, getValueInput.numeraire);
 
-    (uint256 tokenAAmount) = getLiquidityValueAfterArbitrageToPrice(assetToInformation[getValueInput.assetAddress].pair, tokenRates[0], tokenRates[1], getValueInput.assetAmount);
-    //Since Uniswap V2 uses 50/50 pools, it is sufficient to calculate the value of token 0 and multiply with 2 to get the total value of the liquidity position
-    //The token rate is 
-    valueInNumeraire = 2 * FixedPointMathLib.mulDivDown(tokenAAmount, tokenRates[0], FixedPointMathLib.WAD);
+    uint256 token0Amount = getLiquidityValueAfterArbitrageToPrice(assetToInformation[getValueInput.assetAddress].pair, tokenRates[0], tokenRates[1], getValueInput.assetAmount);
+    // Since Uniswap V2 uses 50/50 pools, it is sufficient to calculate the value of token 0 and multiply with 2 to get the total value of the liquidity position.
+    // tokenRates[0] is the value of token0 in a given Numeraire for 1 WAD of tokens, we need to recalculate to find the value
+    // of the actual amount of underlying token0 in the liquidity position.
+    valueInNumeraire = 2 * FixedPointMathLib.mulDivDown(token0Amount, tokenRates[0], FixedPointMathLib.WAD);
 
     return(0, valueInNumeraire);
   }
 
-    // computes the direction and magnitude of the profit-maximizing trade
-    function computeProfitMaximizingTrade(
-        uint256 truePriceTokenA,
-        uint256 truePriceTokenB,
-        uint256 reserveA,
-        uint256 reserveB
-    ) pure internal returns (bool aToB, uint256 amountIn) {
-        aToB = reserveA.mulDivDown(truePriceTokenB, reserveB) < truePriceTokenA;
+    /**
+   * @notice Returns the trusted amount of token0 provided as liquidity, given two trusted prices of token0 and token1
+   * @param pair Address of the Uniswap V2 Liquidity pool
+   * @param trustedPriceToken0 Trusted price of an amount of Token0 in a given Numeraire
+   * @param trustedPriceToken1 Trusted price of an amount of Token1 in a given Numeraire
+   * @param liquidityAmount The amount of LP tokens (ERC20)
+   * @return token0Amount The trusted amount of token0 provided as liquidity
+   * @dev Both trusted prices must be for the same Numeraire, and for an equal amount of tokens
+   *      e.g. if trustedPriceToken0 is the USD price for 10**18 tokens of token0,
+   *      than trustedPriceToken2 must be the USD price for 10**18 tokens of token1.
+   *      The amount of tokens should be big enough to guarantee enough precision for tokens with small unit-prices
+   * @dev The trusted amount of liquidity is calculated by first bringing the liquidity pool in equilibrium,
+   *      by calculating what the reserves of the pool would be if a profit-maximizing trade is done.
+   *      As such flash-loan attacks are mitigated, where an attacker swaps a large amount of the higher priced token,
+   *      to bring the pool out of equilibrium, resulting in liquidity postitions with a higher share of expensive token.
+   * @dev Modification of https://github.com/Uniswap/v2-periphery/blob/master/contracts/libraries/UniswapV2LiquidityMathLibrary.sol#L23
+   */
+    function getLiquidityValueAfterArbitrageToPrice(
+        address pair,
+        uint256 trustedPriceToken0,
+        uint256 trustedPriceToken1,
+        uint256 liquidityAmount
+    ) internal view returns (
+        uint256 token0Amount
+    ) {
+        uint kLast = feeOn ? IUniswapV2Pair(pair).kLast() : 0;
+        uint totalSupply = IUniswapV2Pair(pair).totalSupply();
 
-        uint256 invariant = reserveA * reserveB;
+        // this also checks that totalSupply > 0
+        require(totalSupply >= liquidityAmount && liquidityAmount > 0, 'UV2: LIQUIDITY_AMOUNT');
+
+        (uint reserve0, uint reserve1) = getReservesAfterArbitrage(pair, trustedPriceToken0, trustedPriceToken1);
+
+        return computeLiquidityValue(reserve0, reserve1, totalSupply, liquidityAmount, kLast);
+    }
+
+
+    /**
+   * @notice Gets the reserves after an arbitrage moves the price to the profit-maximizing ratio given externally observed trusted price
+   * @param pair Address of the Uniswap V2 Liquidity pool
+   * @param trustedPriceToken0 Trusted price of an amount of Token0 in a given Numeraire
+   * @param trustedPriceToken1 Trusted price of an amount of Token1 in a given Numeraire
+   * @return reserve0 The reserves of token0 in the liquidity pool after arbitrage
+   * @return reserve1 The reserves of token1 in the liquidity pool after arbitrage
+   * @dev Both trusted prices must be for the same Numeraire, and for an equal amount of tokens
+   *      e.g. if trustedPriceToken0 is the USD price for 10**18 tokens of token0,
+   *      than trustedPriceToken2 must be the USD price for 10**18 tokens of token1.
+   *      The amount of tokens should be big enough to guarantee enough precision for tokens with small unit-prices
+   * @dev Modification of https://github.com/Uniswap/v2-periphery/blob/master/contracts/libraries/UniswapV2LiquidityMathLibrary.sol#L23
+   */
+    function getReservesAfterArbitrage(
+        address pair,
+        uint256 trustedPriceToken0,
+        uint256 trustedPriceToken1
+    ) view internal returns (uint256 reserve0, uint256 reserve1) {
+        // first get reserves before the swap
+        (reserve0, reserve1, ) = IUniswapV2Pair(pair).getReserves();
+
+        require(reserve0 > 0 && reserve1 > 0, 'UV2: ZERO_PAIR_RESERVES');
+
+        // then compute how much to swap to arb to the true price
+        (bool token0ToToken1, uint256 amountIn) = computeProfitMaximizingTrade(trustedPriceToken0, trustedPriceToken1, reserve0, reserve1);
+
+        if (amountIn == 0) {
+            return (reserve0, reserve1);
+        }
+
+        // now affect the trade to the reserves
+        if (token0ToToken1) {
+            uint amountOut = getAmountOut(amountIn, reserve0, reserve1);
+            reserve0 += amountIn;
+            reserve1 -= amountOut;
+        } else {
+            uint amountOut = getAmountOut(amountIn, reserve1, reserve0);
+            reserve1 += amountIn;
+            reserve0 -= amountOut;
+        }
+    }
+
+    /**
+   * @notice Computes the direction and magnitude of the profit-maximizing trade
+   * @param trustedPriceToken0 Trusted price of an amount of Token0 in a given Numeraire
+   * @param trustedPriceToken1 Trusted price of an amount of Token1 in a given Numeraire
+   * @param reserve0 The current reserves of token0 in the liquidity pool
+   * @param reserve1 The current reserves of token1 in the liquidity pool
+   * @return token0ToToken1 The direction of the profit-maximizing trade
+   * @return amountIn The amount of tokens to be swapped of the profit-maximizing trade
+   * @dev Both trusted prices must be for the same Numeraire, and for an equal amount of tokens
+   *      e.g. if trustedPriceToken0 is the USD price for 10**18 tokens of token0,
+   *      than trustedPriceToken2 must be the USD price for 10**18 tokens of token1.
+   *      The amount of tokens should be big enough to guarantee enough precision for tokens with small unit-prices
+   * @dev Modification of https://github.com/Uniswap/v2-periphery/blob/master/contracts/libraries/UniswapV2LiquidityMathLibrary.sol#L23
+   * @dev See https://arxiv.org/pdf/1911.03380.pdf for the derivation:
+   *      - Maximise trustedPriceTokenOut * amountOut - trustedPriceTokenIn * amountIn
+   *      - Constraints:
+   *            * amountIn > 0
+   *            * amountOut > 0
+   *            * Uniswap V2 AMM: (reserveIn + 997 * amountIn / 1000) * (reserveOut - amountOut) = reserveIn * reserveOut
+   *      - Solution (if a profit-maximizing trade exists):
+   *            * sqrt[(1000 * reserveIn * amountOut * trustedPriceTokenOut) / (997 * trustedPriceTokenIn)] - 1000 * reserveIn / 997
+   */
+    function computeProfitMaximizingTrade(
+        uint256 trustedPriceToken0,
+        uint256 trustedPriceToken1,
+        uint256 reserve0,
+        uint256 reserve1
+    ) pure internal returns (bool token0ToToken1, uint256 amountIn) {
+        token0ToToken1 = FixedPointMathLib.mulDivDown(reserve0, trustedPriceToken0, reserve1) < trustedPriceToken1;
+
+        uint256 invariant = reserve0 * reserve1;
 
         uint256 leftSide = FixedPointMathLib.sqrt(
-            invariant * (aToB ? truePriceTokenA : truePriceTokenB) * 1000 /
-            uint256(aToB ? truePriceTokenB : truePriceTokenA) * 997
+            FixedPointMathLib.mulDivDown(
+                invariant * 1000,
+                (token0ToToken1 ? trustedPriceToken1 : trustedPriceToken0),
+                uint256(token0ToToken1 ? trustedPriceToken0 : trustedPriceToken1) * 997
+            )
         );
-        uint256 rightSide = (aToB ? reserveA * 1000 : reserveB * 1000) / 997;
+        uint256 rightSide = (token0ToToken1 ? reserve0 * 1000 : reserve1 * 1000) / 997;
 
         if (leftSide < rightSide) return (false, 0);
 
@@ -92,47 +205,25 @@ contract UniswapV2SubRegistry is SubRegistry {
         amountIn = leftSide - rightSide;
     }
 
-    // gets the reserves after an arbitrage moves the price to the profit-maximizing ratio given an externally observed true price
-    function getReservesAfterArbitrage(
-        address pair,
-        uint256 truePriceTokenA,
-        uint256 truePriceTokenB
-    ) view internal returns (uint256 reserveA, uint256 reserveB) {
-        // first get reserves before the swap
-        (reserveA, reserveB, ) = IUniswapV2Pair(pair).getReserves();
-
-        require(reserveA > 0 && reserveB > 0, 'UniswapV2ArbitrageLibrary: ZERO_PAIR_RESERVES');
-
-        // then compute how much to swap to arb to the true price
-        (bool aToB, uint256 amountIn) = computeProfitMaximizingTrade(truePriceTokenA, truePriceTokenB, reserveA, reserveB);
-
-        if (amountIn == 0) {
-            return (reserveA, reserveB);
-        }
-
-        // now affect the trade to the reserves
-        if (aToB) {
-            uint amountOut = getAmountOut(amountIn, reserveA, reserveB);
-            reserveA += amountIn;
-            reserveB -= amountOut;
-        } else {
-            uint amountOut = getAmountOut(amountIn, reserveB, reserveA);
-            reserveB += amountIn;
-            reserveA -= amountOut;
-        }
-    }
-
-    // computes liquidity value given all the parameters of the pair
+    /**
+   * @notice Computes liquidity value of a LP-position given all the parameters of the pair
+   * @param reserve0 The reserves of token0 in the liquidity pool
+   * @param reserve1 The reserves of token1 in the liquidity pool
+   * @param totalSupply The total supply of LP tokens (ERC20)
+   * @param liquidityAmount The amount of LP tokens (ERC20)
+   * @param kLast The product of the reserves as of the most recent liquidity event (0 if feeOn is false)
+   * @return token0Amount The amount of token0 provided as liquidity
+   * @dev Modification of https://github.com/Uniswap/v2-periphery/blob/master/contracts/libraries/UniswapV2LiquidityMathLibrary.sol#L23
+   */
     function computeLiquidityValue(
-        uint256 reservesA,
-        uint256 reservesB,
+        uint256 reserve0,
+        uint256 reserve1,
         uint256 totalSupply,
         uint256 liquidityAmount,
-        bool feeOn,
         uint kLast
-    ) internal pure returns (uint256 tokenAAmount) {
+    ) internal view returns (uint256 token0Amount) {
         if (feeOn && kLast > 0) {
-            uint rootK = FixedPointMathLib.sqrt(reservesA * reservesB);
+            uint rootK = FixedPointMathLib.sqrt(reserve0 * reserve1);
             uint rootKLast = FixedPointMathLib.sqrt(kLast);
             if (rootK > rootKLast) {
                 uint numerator = totalSupply * (rootK - rootKLast);
@@ -141,35 +232,18 @@ contract UniswapV2SubRegistry is SubRegistry {
                 totalSupply = totalSupply + feeLiquidity;
             }
         }
-        return (reservesA * liquidityAmount / totalSupply);
+        return FixedPointMathLib.mulDivDown(reserve0, liquidityAmount, totalSupply);
     }
 
-    // given two tokens, tokenA and tokenB, and their "true price", i.e. the observed ratio of value of token A to token B,
-    // and a liquidity amount, returns the value of the liquidity in terms of tokenA and tokenB
-    function getLiquidityValueAfterArbitrageToPrice(
-        address pair,
-        uint256 truePriceTokenA,
-        uint256 truePriceTokenB,
-        uint256 liquidityAmount
-    ) internal view returns (
-        uint256 tokenAAmount
-    ) {
-        bool feeOn = IUniswapV2Factory(uniswapV2Factory).feeTo() != address(0);
-        uint kLast = feeOn ? IUniswapV2Pair(pair).kLast() : 0;
-        uint totalSupply = IUniswapV2Pair(pair).totalSupply();
-
-        // this also checks that totalSupply > 0
-        require(totalSupply >= liquidityAmount && liquidityAmount > 0, 'ComputeLiquidityValue: LIQUIDITY_AMOUNT');
-
-        (uint reservesA, uint reservesB) = getReservesAfterArbitrage(pair, truePriceTokenA, truePriceTokenB);
-
-        return computeLiquidityValue(reservesA, reservesB, totalSupply, liquidityAmount, feeOn, kLast);
-    }
-
-    // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
+    /**
+   * @notice Given an input amount of an asset and pair reserves, computes the maximum output amount of the other asset
+   * @param reserveIn The reserves of tokenIn in the liquidity pool
+   * @param reserveOut The reserves of tokenOut in the liquidity pool
+   * @param amountIn The input amount of tokenIn
+   * @return amountOut The output amount of tokenIn
+   * @dev Derived from Uniswap V2 AMM: (reserveIn + 997 * amountIn / 1000) * (reserveOut - amountOut) = reserveIn * reserveOut
+   */
     function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
-        require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
-        require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
         uint amountInWithFee = amountIn * 997;
         uint numerator = amountInWithFee * reserveOut;
         uint denominator = reserveIn * 1000 + amountInWithFee;
