e32b234ed50661c119efd3c192f4b72b52558738
Jasper Van Pee <jaspervanpee@gmail.com>
2022-06-08 18:46:55 +0200

    📄 change license headers to work with foundry.fmt

--- src/interfaces/ISubRegistry.sol
+++ src/interfaces/ISubRegistry.sol
@@ -1,7 +1,11 @@
-// This is a private, unpublished repository.
-// All rights reserved to Arcadia Finance.
-// Any modification, publication, reproduction, commercialisation, incorporation, sharing or any other kind of use of any part of this code or derivatives thereof is not allowed.
-// SPDX-License-Identifier: UNLICENSED
+/** 
+    This is a private, unpublished repository.
+    All rights reserved to Arcadia Finance.
+    Any modification, publication, reproduction, commercialisation, incorporation, 
+    sharing or any other kind of use of any part of this code or derivatives thereof is not allowed.
+    
+    SPDX-License-Identifier: UNLICENSED
+ */
 pragma solidity >=0.4.22 <0.9.0;
 
 interface ISubRegistry {
