affe48ae6b145088fae66140ca60784a3a7d382e
Thomas Smets <thomass-smets@hotmail.com>
2022-06-08 19:14:57 +0200

    🎨 Change formatting Uniswap interfaces

--- src/interfaces/IUniswapV2Pair.sol
+++ src/interfaces/IUniswapV2Pair.sol
@@ -1,14 +1,30 @@
-// This is a private, unpublished repository.
-// All rights reserved to Arcadia Finance.
-// Any modification, publication, reproduction, commercialisation, incorporation, sharing or any other kind of use of any part of this code or derivatives thereof is not allowed.
-// SPDX-License-Identifier: UNLICENSED
+/** 
+    This is a private, unpublished repository.
+    All rights reserved to Arcadia Finance.
+    Any modification, publication, reproduction, commercialization, incorporation, 
+    sharing or any other kind of use of any part of this code or derivatives thereof is not allowed.
+    
+    SPDX-License-Identifier: UNLICENSED
+ */
 pragma solidity >=0.4.22 <0.9.0;
 
 interface IUniswapV2Pair {
     function totalSupply() external view returns (uint256);
+
     function decimals() external pure returns (uint8);
+
     function token0() external view returns (address);
+
     function token1() external view returns (address);
-    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+
+    function getReserves() 
+        external 
+        view 
+        returns (
+            uint112 reserve0,
+            uint112 reserve1,
+            uint32 blockTimestampLast
+        );
+
     function kLast() external view returns (uint);
 }
