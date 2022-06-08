64e366977e817637ba77d59978cecc0ea7b6b07c
Thomas Smets <thomass-smets@hotmail.com>
2022-06-08 18:46:50 +0200

    🎨 Format IMainRegistry

--- src/interfaces/IMainRegistry.sol
+++ src/interfaces/IMainRegistry.sol
@@ -5,19 +5,33 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 interface IMainRegistry {
-  function addAsset(address, uint256[] memory) external;
-  function getTotalValue(
-              address[] calldata _assetAddresses, 
-              uint256[] calldata _assetIds,
-              uint256[] calldata _assetAmounts,
-              uint256 numeraire
-            ) external view returns (uint256);
-  function getListOfValuesPerAsset(
-              address[] calldata _assetAddresses, 
-              uint256[] calldata _assetIds,
-              uint256[] calldata _assetAmounts,
-              uint256 numeraire
-            ) external view returns (uint256[] memory);
-  function factoryAddress() external view returns (address);
-  function numeraireToInformation(uint256 numeraire) external view returns (uint64, uint64, address, address, address, string memory);
+    function addAsset(address, uint256[] memory) external;
+
+    function getTotalValue(
+        address[] calldata _assetAddresses,
+        uint256[] calldata _assetIds,
+        uint256[] calldata _assetAmounts,
+        uint256 numeraire
+    ) external view returns (uint256);
+
+    function getListOfValuesPerAsset(
+        address[] calldata _assetAddresses, 
+        uint256[] calldata _assetIds,
+        uint256[] calldata _assetAmounts,
+        uint256 numeraire
+    ) external view returns (uint256[] memory);
+
+    function factoryAddress() external view returns (address);
+
+    function numeraireToInformation(uint256 numeraire)
+        external
+        view
+        returns (
+            uint64,
+            uint64,
+            address,
+            address,
+            address,
+            string memory
+        );
 }
