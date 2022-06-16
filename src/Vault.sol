865ec227d68db9360e0478f3eb9fde3934f47faa
Jasper Van Pee <jaspervanpee@gmail.com>
2022-06-16 09:58:07 +0200

    🚑️ store numeraire ID on vault

--- src/Vault.sol
+++ src/Vault.sol
@@ -147,6 +147,7 @@ contract Vault {
     function initialize(
         address _owner,
         address registryAddress,
+        uint8 numeraire,
         address stable,
         address stakeContract,
         address irmAddress
@@ -156,6 +157,7 @@ contract Vault {
         owner = _owner;
         debt._collThres = 150;
         debt._liqThres = 110;
+        debt._numeraire = numeraire;
         _stable = stable;
         _stakeContract = stakeContract;
         _irmAddress = irmAddress;
