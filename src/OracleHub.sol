d53e1cd33b54a25ee299a870844b4fd0a6b8c390
Thomas Smets <thomass-smets@hotmail.com>
2022-06-22 09:23:23 +0200

    🔥 Remove Printer from Oraclehub

--- src/OracleHub.sol
+++ src/OracleHub.sol
@@ -10,8 +10,6 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import "./interfaces/IChainLinkData.sol";
-
-import {Printing} from "./utils/Printer.sol";
 import {FixedPointMathLib} from "./utils/FixedPointMathLib.sol";
 
 /**
