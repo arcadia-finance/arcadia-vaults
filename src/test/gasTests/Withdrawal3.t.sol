84bceb6a5a5b46d71282d568e7af188fef50614b
Jasper Van Pee <jaspervanpee@gmail.com>
2022-07-11 11:35:51 +0200

    📄 licenses & readme init

--- src/test/gasTests/Withdrawal3.t.sol
+++ src/test/gasTests/Withdrawal3.t.sol
@@ -1,4 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
+/** 
+    Created by Arcadia Finance
+    https://www.arcadia.finance
+
+    SPDX-License-Identifier: BUSL-1.1
+ */
 pragma solidity >0.8.10;
 
 import "../../../lib/forge-std/src/Test.sol";
