ad3530d7a0752534dde6ba8fb79cf83cb2e9e949
Simon L <simon.luyten@gmail.com>
2022-08-30 11:37:44 +0200

    :fire: remove imports and move decimals view func

--- src/interfaces/IERC4626.sol
+++ src/interfaces/IERC4626.sol
@@ -1,18 +1,8 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.7.0) (interfaces/IERC4626.sol)
-
 pragma solidity ^0.8.0;
 
-import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-
-/**
- * @dev Interface of the ERC4626 "Tokenized Vault Standard", as defined in
- * https://eips.ethereum.org/EIPS/eip-4626[ERC-4626].
- *
- * _Available since v4.7._
- */
-interface IERC4626 is IERC20, IERC20Metadata {
+interface IERC4626 {
     function asset() external view returns (address assetTokenAddress);
+    function decimals() external view returns (uint256 decimals);
     function convertToAssets(uint256 shares) external view returns (uint256 assets);
 }
