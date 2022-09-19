caacfa5385454e135b86b8428ab54772e68272a4
Thomas Smets <thomass-smets@hotmail.com>
2022-09-19 19:51:40 +0200

    🎨 Set isTrustedProtocolSet to visible

--- src/Vault.sol
+++ src/Vault.sol
@@ -38,7 +38,7 @@ contract Vault {
      */
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
-    bool isTrustedProtocolSet;
+    bool public isTrustedProtocolSet;
 
     uint16 public vaultVersion;
     uint256 public life;
