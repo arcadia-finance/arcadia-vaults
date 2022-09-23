193117d60941045d2b58fcc502b36ff828fc4b62
Thomas Smets <thomass-smets@hotmail.com>
2022-09-23 16:59:41 +0200

    📝 Fix typos

--- src/AssetRegistry/FloorERC1155PricingModule.sol
+++ src/AssetRegistry/FloorERC1155PricingModule.sol
@@ -100,8 +100,8 @@ contract FloorERC1155PricingModule is PricingModule {
      * - baseCurrency: The BaseCurrency (base-asset) in which the value is ideally expressed
      * @return valueInUsd The value of the asset denominated in USD with 18 Decimals precision
      * @return valueInBaseCurrency The value of the asset denominated in BaseCurrency different from USD with 18 Decimals precision
-     * @dev If the Oracle-Hub returns the rate in a baseCurrency different from USD, the StandardERC20Registry will return
-     * the value of the asset in the same BaseCurrency. If the Oracle-Hub returns the rate in USD, the StandardERC20Registry
+     * @dev If the Oracle-Hub returns the rate in a baseCurrency different from USD, the FloorERC1155PricingModule will return
+     * the value of the asset in the same BaseCurrency. If the Oracle-Hub returns the rate in USD, the FloorERC1155PricingModule
      * will return the value of the asset in USD.
      * Only one of the two values can be different from 0.
      * @dev Function will overflow when assetAmount * Rate * 10**(18 - rateDecimals) > MAXUINT256
