f195c5b3546d1c5e55e816a2194d8408e32a8e33
Thomas Smets <thomass-smets@hotmail.com>
2022-09-26 19:37:17 +0200

    ♻️ Refactor UniswapV2PricingModule

--- src/AssetRegistry/UniswapV2PricingModule.sol
+++ src/AssetRegistry/UniswapV2PricingModule.sol
@@ -1,10 +1,8 @@
-/** 
-    This is a private, unpublished repository.
-    All rights reserved to Arcadia Finance.
-    Any modification, publication, reproduction, commercialization, incorporation, 
-    sharing or any other kind of use of any part of this code or derivatives thereof is not allowed.
-    
-    SPDX-License-Identifier: UNLICENSED
+/**
+ * Created by Arcadia Finance
+ * https://www.arcadia.finance
+ *
+ * SPDX-License-Identifier: BUSL-1.1
  */
 pragma solidity >=0.4.22 <0.9.0;
 
@@ -19,7 +17,7 @@ import {PRBMath} from "../utils/PRBMath.sol";
  * @author Arcadia Finance
  * @notice The UniswapV2PricingModule stores pricing logic and basic information for Uniswap V2 LP tokens
  * @dev No end-user should directly interact with the UniswapV2PricingModule, only the Main-registry, Oracle-Hub or the contract owner
- * @dev Most logic in this contract is a modifications of 
+ * @dev Most logic in this contract is a modifications of
  *      https://github.com/Uniswap/v2-periphery/blob/master/contracts/libraries/UniswapV2LiquidityMathLibrary.sol#L23
  */
 contract UniswapV2PricingModule is PricingModule {
@@ -29,28 +27,30 @@ contract UniswapV2PricingModule is PricingModule {
     uint256 public constant poolUnit = 1000000000000000000;
     address public immutable uniswapV2Factory;
 
-    bool feeOn;
+    bool public feeOn;
+
+    mapping(address => AssetInformation) public assetToInformation;
 
     struct AssetInformation {
         address token0;
         address token1;
     }
 
-    mapping(address => AssetInformation) public assetToInformation;
-
     /**
      * @notice A Pricing-Module must always be initialised with the address of the Main-Registry and of the Oracle-Hub
      * @param _mainRegistry The address of the Main-registry
-     * @param _oracleHub The address of the Oracle-Hub 
+     * @param _oracleHub The address of the Oracle-Hub
      * @param _uniswapV2Factory The factory for Uniswap V2 pairs
      */
-    constructor(
-        address _mainRegistry,
-        address _oracleHub,
-        address _uniswapV2Factory
-    ) PricingModule(_mainRegistry, _oracleHub) {
+    constructor(address _mainRegistry, address _oracleHub, address _uniswapV2Factory)
+        PricingModule(_mainRegistry, _oracleHub)
+    {
         uniswapV2Factory = _uniswapV2Factory;
     }
+
+    /*///////////////////////////////////////////////////////////////
+                        UNISWAP V2 FEE
+    ///////////////////////////////////////////////////////////////*/
 
     /**
      * @notice Fetches boolean on the uniswap factory if fees are enabled or not
@@ -58,6 +58,10 @@ contract UniswapV2PricingModule is PricingModule {
     function syncFee() external {
         feeOn = IUniswapV2Factory(uniswapV2Factory).feeTo() != address(0);
     }
+
+    /*///////////////////////////////////////////////////////////////
+                        ASSET MANAGEMENT
+    ///////////////////////////////////////////////////////////////*/
 
     /**
      * @notice Adds a new asset to the UniswapV2PricingModule, or overwrites an existing asset.
@@ -72,10 +76,7 @@ contract UniswapV2PricingModule is PricingModule {
      *      This risk can be mitigated by setting the boolean "assetsUpdatable" in the MainRegistry to false, after which
      *      assets are no longer updatable.
      */
-    function setAssetInformation(
-        address assetAddress,
-        uint256[] calldata assetCreditRatings
-    ) external onlyOwner {
+    function setAssetInformation(address assetAddress, uint256[] calldata assetCreditRatings) external onlyOwner {
         AssetInformation memory assetInformation;
 
         assetInformation.token0 = IUniswapV2Pair(assetAddress).token0();
@@ -97,18 +98,17 @@ contract UniswapV2PricingModule is PricingModule {
         IMainRegistry(mainRegistry).addAsset(assetAddress, assetCreditRatings);
     }
 
+    /*///////////////////////////////////////////////////////////////
+                        WHITE LIST MANAGEMENT
+    ///////////////////////////////////////////////////////////////*/
+
     /**
      * @notice Checks for a token address and the corresponding Id if it is white-listed
      * @param assetAddress The address of the asset
      * @dev Since Uniswap V2 LP tokens (ERC20) have no Id, the Id should be set to 0
      * @return A boolean, indicating if the asset passed as input is whitelisted
      */
-    function isWhiteListed(address assetAddress, uint256)
-        external
-        view
-        override
-        returns (bool)
-    {
+    function isWhiteListed(address assetAddress, uint256) external view override returns (bool) {
         if (isAssetAddressWhiteListed[assetAddress]) {
             return true;
         }
@@ -116,20 +116,33 @@ contract UniswapV2PricingModule is PricingModule {
         return false;
     }
 
+    /*///////////////////////////////////////////////////////////////
+                          PRICING LOGIC
+    ///////////////////////////////////////////////////////////////*/
+
     /**
      * @notice Returns the value of a certain asset, denominated in a given BaseCurrency
      * @param getValueInput A Struct with all the information neccessary to get the value of an asset
+     * - assetAddress: The contract address of the asset
+     * - assetId: Since ERC20 tokens have no Id, the Id should be set to 0
+     * - assetAmount: The Amount of tokens, ERC20 tokens can have any Decimals precision smaller than 18.
+     * - baseCurrency: The BaseCurrency (base-asset) in which the value is ideally expressed
      * @return valueInUsd The value of the asset denominated in USD with 18 Decimals precision
      * @return valueInBaseCurrency The value of the asset denominated in BaseCurrency different from USD with 18 Decimals precision
+     * @dev If the Oracle-Hub returns the rate in a baseCurrency different from USD, the StandardERC20PricingModule will return
+     * the value of the asset in the same BaseCurrency. If the Oracle-Hub returns the rate in USD, the StandardERC20PricingModule
+     * will return the value of the asset in USD.
+     * Only one of the two values can be different from 0.
+     * @dev Function will overflow when assetAmount * Rate * 10**(18 - rateDecimals) > MAXUINT256
+     * @dev If the asset is not first added to PricingModule this function will return value 0 without throwing an error.
+     * However no check in StandardERC20PricingModule is necessary, since the check if the asset is whitelisted (and hence added to PricingModule)
+     * is already done in the Main-Registry.
      */
     function getValue(GetValueInput memory getValueInput)
         public
         view
         override
-        returns (
-            uint256 valueInUsd,
-            uint256 valueInBaseCurrency
-        )
+        returns (uint256 valueInUsd, uint256 valueInBaseCurrency)
     {
         address[] memory tokens = new address[](2);
         tokens[0] = assetToInformation[getValueInput.assetAddress].token0;
@@ -140,14 +153,19 @@ contract UniswapV2PricingModule is PricingModule {
         tokenAmounts[0] = FixedPointMathLib.WAD;
         tokenAmounts[1] = FixedPointMathLib.WAD;
 
-        uint256[] memory tokenRates = IMainRegistry(mainRegistry).getListOfValuesPerAsset(tokens, new uint256[](2), tokenAmounts, getValueInput.baseCurrency);
+        uint256[] memory tokenRates = IMainRegistry(mainRegistry).getListOfValuesPerAsset(
+            tokens, new uint256[](2), tokenAmounts, getValueInput.baseCurrency
+        );
 
-        (uint256 token0Amount, uint256 token1Amount) = getLiquidityValueAfterArbitrageToPrice(getValueInput.assetAddress, tokenRates[0], tokenRates[1], getValueInput.assetAmount);
+        (uint256 token0Amount, uint256 token1Amount) = getLiquidityValueAfterArbitrageToPrice(
+            getValueInput.assetAddress, tokenRates[0], tokenRates[1], getValueInput.assetAmount
+        );
         // tokenRates[0] is the value of token0 in a given BaseCurrency with 18 decimals precision for 1 WAD of tokens,
         // we need to recalculate to find the value of the actual amount of underlying token0 in the liquidity position.
-        valueInBaseCurrency = FixedPointMathLib.mulDivDown(token0Amount, tokenRates[0], FixedPointMathLib.WAD) + FixedPointMathLib.mulDivDown(token1Amount, tokenRates[1], FixedPointMathLib.WAD);
+        valueInBaseCurrency = FixedPointMathLib.mulDivDown(token0Amount, tokenRates[0], FixedPointMathLib.WAD)
+            + FixedPointMathLib.mulDivDown(token1Amount, tokenRates[1], FixedPointMathLib.WAD);
 
-        return(0, valueInBaseCurrency);
+        return (0, valueInBaseCurrency);
     }
 
     /**
@@ -174,13 +192,13 @@ contract UniswapV2PricingModule is PricingModule {
         uint256 trustedPriceToken1,
         uint256 liquidityAmount
     ) internal view returns (uint256 token0Amount, uint256 token1Amount) {
-        uint kLast = feeOn ? IUniswapV2Pair(pair).kLast() : 0;
-        uint totalSupply = IUniswapV2Pair(pair).totalSupply();
+        uint256 kLast = feeOn ? IUniswapV2Pair(pair).kLast() : 0;
+        uint256 totalSupply = IUniswapV2Pair(pair).totalSupply();
 
         // this also checks that totalSupply > 0
-        require(totalSupply >= liquidityAmount && liquidityAmount > 0, 'UV2_GV: LIQUIDITY_AMOUNT');
+        require(totalSupply >= liquidityAmount && liquidityAmount > 0, "UV2_GV: LIQUIDITY_AMOUNT");
 
-        (uint reserve0, uint reserve1) = getReservesAfterArbitrage(pair, trustedPriceToken0, trustedPriceToken1);
+        (uint256 reserve0, uint256 reserve1) = getReservesAfterArbitrage(pair, trustedPriceToken0, trustedPriceToken1);
 
         return computeLiquidityValue(reserve0, reserve1, totalSupply, liquidityAmount, kLast);
     }
@@ -198,18 +216,19 @@ contract UniswapV2PricingModule is PricingModule {
      *      The amount of tokens should be big enough to guarantee enough precision for tokens with small unit-prices
      * @dev Modification of https://github.com/Uniswap/v2-periphery/blob/master/contracts/libraries/UniswapV2LiquidityMathLibrary.sol#L23
      */
-    function getReservesAfterArbitrage(
-        address pair,
-        uint256 trustedPriceToken0,
-        uint256 trustedPriceToken1
-    ) internal view returns (uint256 reserve0, uint256 reserve1) {
+    function getReservesAfterArbitrage(address pair, uint256 trustedPriceToken0, uint256 trustedPriceToken1)
+        internal
+        view
+        returns (uint256 reserve0, uint256 reserve1)
+    {
         // first get reserves before the swap
-        (reserve0, reserve1, ) = IUniswapV2Pair(pair).getReserves();
+        (reserve0, reserve1,) = IUniswapV2Pair(pair).getReserves();
 
-        require(reserve0 > 0 && reserve1 > 0, 'UV2_GV: ZERO_PAIR_RESERVES');
+        require(reserve0 > 0 && reserve1 > 0, "UV2_GV: ZERO_PAIR_RESERVES");
 
         // then compute how much to swap to arb to the true price
-        (bool token0ToToken1, uint256 amountIn) = computeProfitMaximizingTrade(trustedPriceToken0, trustedPriceToken1, reserve0, reserve1);
+        (bool token0ToToken1, uint256 amountIn) =
+            computeProfitMaximizingTrade(trustedPriceToken0, trustedPriceToken1, reserve0, reserve1);
 
         if (amountIn == 0) {
             return (reserve0, reserve1);
@@ -217,11 +236,11 @@ contract UniswapV2PricingModule is PricingModule {
 
         // now affect the trade to the reserves
         if (token0ToToken1) {
-            uint amountOut = getAmountOut(amountIn, reserve0, reserve1);
+            uint256 amountOut = getAmountOut(amountIn, reserve0, reserve1);
             reserve0 += amountIn;
             reserve1 -= amountOut;
         } else {
-            uint amountOut = getAmountOut(amountIn, reserve1, reserve0);
+            uint256 amountOut = getAmountOut(amountIn, reserve1, reserve0);
             reserve1 += amountIn;
             reserve0 -= amountOut;
         }
@@ -250,7 +269,7 @@ contract UniswapV2PricingModule is PricingModule {
      *            * amountIn = sqrt[(1000 * reserveIn * amountOut * trustedPriceTokenOut) / (997 * trustedPriceTokenIn)] - 1000 * reserveIn / 997 (if a profit-maximizing trade exists)
      *            * amountIn = 0 (if a profit-maximizing trade does not exists)
      * @dev Function overflows (and reverts) if reserve0 * trustedPriceToken0 > max uint256, however this is not possible in realistic scenario's
-     *      This can only happen if trustedPriceToken0 is bigger than 2.23 * 10^43 
+     *      This can only happen if trustedPriceToken0 is bigger than 2.23 * 10^43
      *      (for an asset with 0 decimals and reserve0 Max uint112 this would require a unit price of $2.23 * 10^7
      */
     function computeProfitMaximizingTrade(
@@ -300,12 +319,12 @@ contract UniswapV2PricingModule is PricingModule {
         uint256 kLast
     ) internal view returns (uint256 token0Amount, uint256 token1Amount) {
         if (feeOn && kLast > 0) {
-            uint rootK = FixedPointMathLib.sqrt(reserve0 * reserve1);
-            uint rootKLast = FixedPointMathLib.sqrt(kLast);
+            uint256 rootK = FixedPointMathLib.sqrt(reserve0 * reserve1);
+            uint256 rootKLast = FixedPointMathLib.sqrt(kLast);
             if (rootK > rootKLast) {
-                uint numerator = totalSupply * (rootK - rootKLast);
-                uint denominator = rootK * 5 + rootKLast;
-                uint feeLiquidity = numerator / denominator;
+                uint256 numerator = totalSupply * (rootK - rootKLast);
+                uint256 denominator = rootK * 5 + rootKLast;
+                uint256 feeLiquidity = numerator / denominator;
                 totalSupply = totalSupply + feeLiquidity;
             }
         }
@@ -324,14 +343,14 @@ contract UniswapV2PricingModule is PricingModule {
      * @dev Derived from Uniswap V2 AMM equation:
      *      (reserveIn + 997 * amountIn / 1000) * (reserveOut - amountOut) = reserveIn * reserveOut
      */
-    function getAmountOut(
-        uint256 amountIn,
-        uint256 reserveIn,
-        uint256 reserveOut
-    ) internal pure returns (uint256 amountOut) {
-        uint amountInWithFee = amountIn * 997;
-        uint numerator = amountInWithFee * reserveOut;
-        uint denominator = reserveIn * 1000 + amountInWithFee;
+    function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut)
+        internal
+        pure
+        returns (uint256 amountOut)
+    {
+        uint256 amountInWithFee = amountIn * 997;
+        uint256 numerator = amountInWithFee * reserveOut;
+        uint256 denominator = reserveIn * 1000 + amountInWithFee;
         amountOut = numerator / denominator;
     }
 }
