cd222efee0f5ed75b3c0d2d6583eb134b44430bd
Simon L <simon.luyten@gmail.com>
2022-10-27 13:49:04 +0200

    :art: change descirptipn of asset Ids

--- src/AssetManagement/utils/ActionAssetData.sol
+++ src/AssetManagement/utils/ActionAssetData.sol
@@ -8,7 +8,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 struct actionAssetsData {
     address[] assets; // Assets addresses
-    uint256[] assetIds; // Arcadia Protocol asset ids (internal ids given to arcadia's whitelisted assets)
+    uint256[] assetIds; // Arcadia Protocol asset ids of asset types (0 = erc20 ...)
     uint256[] assetAmounts; // Action asset amounts
     uint256[] preActionBalances; // Account asset balances pre action
 }
