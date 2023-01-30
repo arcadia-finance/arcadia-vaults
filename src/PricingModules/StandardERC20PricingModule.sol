36823947b8615d5659508b4eb02c621080763e72
zeki <zeki@arcadia.finance>
2023-01-30 17:56:03 +0100

    :recycle: Refactor comment 80

--- src/PricingModules/StandardERC20PricingModule.sol
+++ src/PricingModules/StandardERC20PricingModule.sol
@@ -79,7 +79,6 @@ contract StandardERC20PricingModule is PricingModule {
 
     /**
      * @notice Returns the information that is stored in the StandardERC20PricingModule for a given ERC20 token.
-     * @dev struct is not taken into memory; saves gas.
      * @param asset The Token address of the asset.
      * @return assetUnit The unit (10 ** decimals) of the asset.
      * @return oracles The list of addresses of the oracles to get the exchange rate of the asset in USD.
