6c014425515ba6db6004715dda6ced981118fbcc
zeki <zeki@arcadia.finance>
2023-01-30 17:53:46 +0100

    :pencil2: pragma

--- src/PricingModules/StandardERC20PricingModule.sol
+++ src/PricingModules/StandardERC20PricingModule.sol
@@ -4,7 +4,7 @@
  *
  * SPDX-License-Identifier: BUSL-1.1
  */
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.13;
 
 import {PricingModule} from "./AbstractPricingModule.sol";
 import {IOraclesHub} from "./interfaces/IOraclesHub.sol";
