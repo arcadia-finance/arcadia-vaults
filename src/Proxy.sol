a0bc3ba0d1432a3032efcd3381ddab8c545cc13c
Thomas Smets <thomass-smets@hotmail.com>
2023-01-30 18:46:00 +0100

    📝 Small typo

--- src/Proxy.sol
+++ src/Proxy.sol
@@ -10,7 +10,7 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Proxy {
     /**
      * @dev Storage slot with the address of the current implementation.
-     * This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1, and is
+     * This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1.
      */
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
