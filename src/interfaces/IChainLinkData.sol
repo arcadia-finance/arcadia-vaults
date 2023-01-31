458bff1e9cfb65a405793a0696278b7208723b62
zekiblue <zeki@arcadia.finance>
2023-01-31 20:06:32 +0100

    :truck: Move chainlink aggregator interface

--- src/interfaces/IChainLinkData.sol
+++ src/interfaces/IChainLinkData.sol
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 
+// @notice Interface for ChainLink Data Feed, it is based on https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol
 interface IChainLinkData {
-    function latestRoundData()
+    function decimals() external view returns (uint8);
+
+    function description() external view returns (string memory);
+
+    function version() external view returns (uint256);
+
+    function getRoundData(uint80 _roundId)
         external
         view
         returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 
-    function aggregator() external view returns (address);
-
-    function minAnswer() external view returns (int192);
+    function latestRoundData()
+        external
+        view
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 }
