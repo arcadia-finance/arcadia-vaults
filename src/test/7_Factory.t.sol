fd8a6de3ed1b502b32542aff2c92c6fc8bf95fe7
Thomas Smets <thomass-smets@hotmail.com>
2023-01-31 20:59:21 +0100

    🐛 fix failing test

--- src/test/7_Factory.t.sol
+++ src/test/7_Factory.t.sol
@@ -492,6 +492,7 @@ contract FactoryTest is DeployArcadiaVaults {
         address logic
     ) public {
         vm.assume(logic != address(0));
+        vm.assume(randomAssetAddress != address(0));
         vm.assume(randomAssetAddress != address(eth));
         assertEq(false, factory.newVaultInfoSet());
 
