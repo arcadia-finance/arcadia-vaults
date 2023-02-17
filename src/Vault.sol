1d7da401af45772e6818c7783691f7de4d61b128
Thomas Smets <thomass-smets@hotmail.com>
2023-02-17 19:20:57 +0100

    🐛 reset trustedCreditor on Liquidation and AssetManager allowances depend on vault owner

--- src/Vault.sol
+++ src/Vault.sol
@@ -57,7 +57,7 @@ contract Vault is IVault {
     uint256[] public erc721TokenIds;
     uint256[] public erc1155TokenIds;
 
-    mapping(address => bool) public isAssetManager;
+    mapping(address => mapping(address => bool)) public isAssetManager;
 
     struct AddressSlot {
         address value;
@@ -87,7 +87,8 @@ contract Vault is IVault {
      */
     modifier onlyAssetManager() {
         require(
-            msg.sender == owner || msg.sender == trustedCreditor || isAssetManager[msg.sender], "V: Only Asset Manager"
+            msg.sender == owner || msg.sender == trustedCreditor || isAssetManager[owner][msg.sender],
+            "V: Only Asset Manager"
         );
         _;
     }
@@ -411,7 +412,14 @@ contract Vault is IVault {
         originalOwner = owner;
         _transferOwnership(msg.sender);
 
-        return (originalOwner, baseCurrency, trustedCreditor);
+        //Cashe trustedCreditor
+        trustedCreditor_ = trustedCreditor;
+
+        //Close margin account
+        isTrustedCreditorSet = false;
+        trustedCreditor = address(0);
+
+        return (originalOwner, baseCurrency, trustedCreditor_);
     }
 
     /*///////////////////////////////////////////////////////////////
@@ -429,7 +437,7 @@ contract Vault is IVault {
      * - Chain interactions with the Trusted Creditor together with vault actions (eg. borrow deposit and trade in one transaction).
      */
     function setAssetManager(address assetManager, bool value) external onlyOwner {
-        isAssetManager[assetManager] = value;
+        isAssetManager[msg.sender][assetManager] = value;
     }
 
     /**
