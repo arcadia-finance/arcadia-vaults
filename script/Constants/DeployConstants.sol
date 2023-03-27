454760ee9938a29ddc1c57a3c688c9149fe6780e
Jasper Van Pee <jaspervanpee@gmail.com>
2023-03-27 12:14:54 +0200

    🔥 cleanup & comments

--- script/Constants/DeployConstants.sol
+++ script/Constants/DeployConstants.sol
@@ -5,20 +5,6 @@
 pragma solidity ^0.8.13;
 
 library DeployAddresses {
-    // address public constant dai = 0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844;
-    // address public constant eth = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
-    // address public constant link = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
-    // address public constant snx = 0x51f44ca59b867E005e48FA573Cb8df83FC7f7597;
-    // address public constant usdc = 0x07865c6E87B9F70255377e024ace6630C1Eaa37F;
-    // address public constant btc = 0xC04B0d3107736C32e19F1c62b2aF67BE61d63a05;
-
-    // address public constant oracleDaiToUsd = 0x0d79df66BE487753B02D015Fb622DED7f0E9798d;
-    // address public constant oracleEthToUsd = 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e;
-    // address public constant oracleLinkToEth = 0xb4c4a493AB6356497713A78FFA6c60FB53517c63;
-    // address public constant oracleSnxToUsd = 0xdC5f59e61e51b90264b38F0202156F07956E2577;
-    // address public constant oracleUsdcToUsd = 0xAb5c49580294Aff77670F839ea425f5b78ab3Ae7;
-    // address public constant oracleBtcToEth = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
-
     address public constant eth_optimism = 0x4200000000000000000000000000000000000006;
 
     // MAINNET
@@ -125,9 +111,9 @@ library DeployRiskConstantsMainnet {
     uint16 public constant crv_liqFact_1 = 85; //eth
     uint16 public constant crv_liqFact_2 = 85; //usdc
 
-    uint16 public constant dai_collFact_1 = 91; //eth
+    uint16 public constant dai_collFact_1 = 86; //eth
     uint16 public constant dai_collFact_2 = 91; //usdc
-    uint16 public constant dai_liqFact_1 = 96; //eth
+    uint16 public constant dai_liqFact_1 = 92; //eth
     uint16 public constant dai_liqFact_2 = 96; //usdc
 
     uint16 public constant frax_collFact_1 = 86; //eth
@@ -182,9 +168,9 @@ library DeployRiskConstantsMainnet {
 }
 
 library DeployRiskConstantsOptimism {
-    uint16 public constant dai_collFact_1 = 91; //eth
+    uint16 public constant dai_collFact_1 = 86; //eth
     uint16 public constant dai_collFact_2 = 91; //usdc
-    uint16 public constant dai_liqFact_1 = 96; //eth
+    uint16 public constant dai_liqFact_1 = 92; //eth
     uint16 public constant dai_liqFact_2 = 96; //usdc
 
     uint16 public constant frax_collFact_1 = 76; //eth
