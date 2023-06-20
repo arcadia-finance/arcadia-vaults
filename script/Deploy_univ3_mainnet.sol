70011a7da8d961ed7460aebc82e7def7b928945d
j-vp <jasper@arcadia.finance>
2023-06-20 10:21:04 +0200

    🔒️ set exposure in deploy script

--- script/Deploy_univ3_mainnet.sol
+++ script/Deploy_univ3_mainnet.sol
@@ -5,7 +5,7 @@
 pragma solidity ^0.8.13;
 
 import "../lib/forge-std/src/Test.sol";
-import { DeployAddresses } from "./Constants/DeployConstants.sol";
+import { DeployAddresses, DeployNumbers } from "./Constants/DeployConstants.sol";
 
 import { MainRegistry } from "../src/MainRegistry.sol";
 import { StandardERC20PricingModule } from "../src/PricingModules/StandardERC20PricingModule.sol";
@@ -39,19 +39,45 @@ contract ArcadiaUniV3DeployerMainnet is Test {
         mainRegistry.addPricingModule(address(uniV3PricingModule));
         uniV3PricingModule.addAsset(DeployAddresses.uniswapV3PositionMgr_mainnet);
 
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.crv_mainnet, type(uint128).max);
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.dai_mainnet, type(uint128).max);
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.frax_mainnet, type(uint128).max);
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.fxs_mainnet, type(uint128).max);
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.link_mainnet, type(uint128).max);
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.snx_mainnet, type(uint128).max);
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.uni_mainnet, type(uint128).max);
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.usdc_mainnet, type(uint128).max);
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.usdt_mainnet, type(uint128).max);
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.wbtc_mainnet, type(uint128).max);
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.weth_mainnet, type(uint128).max);
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.cbeth_mainnet, type(uint128).max);
-        uniV3PricingModule.setExposureOfAsset(DeployAddresses.reth_mainnet, type(uint128).max);
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.crv_mainnet, uint128(6_000_000 * 10 ** DeployNumbers.crvDecimals)
+        );
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.dai_mainnet, uint128(75_000_000 * 10 ** DeployNumbers.daiDecimals)
+        );
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.frax_mainnet, uint128(35_000_000 * 10 ** DeployNumbers.fraxDecimals)
+        );
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.fxs_mainnet, uint128(300_000 * 10 ** DeployNumbers.fxsDecimals)
+        );
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.link_mainnet, uint128(6_500_000 * 10 ** DeployNumbers.linkDecimals)
+        );
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.snx_mainnet, uint128(300_000 * 10 ** DeployNumbers.snxDecimals)
+        );
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.uni_mainnet, uint128(600_000 * 10 ** DeployNumbers.uniDecimals)
+        );
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.usdc_mainnet, uint128(75_000_000 * 10 ** DeployNumbers.usdcDecimals)
+        );
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.usdt_mainnet, uint128(70_000_000 * 10 ** DeployNumbers.usdtDecimals)
+        );
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.wbtc_mainnet, uint128(3500 * 10 ** DeployNumbers.wbtcDecimals)
+        );
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.weth_mainnet, uint128(50_000 * 10 ** DeployNumbers.wethDecimals)
+        );
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.cbeth_mainnet, uint128(2250 * 10 ** DeployNumbers.cbethDecimals)
+        );
+        uniV3PricingModule.setExposureOfAsset(
+            DeployAddresses.reth_mainnet, uint128(35_000 * 10 ** DeployNumbers.rethDecimals)
+        );
 
         vm.stopBroadcast();
     }
